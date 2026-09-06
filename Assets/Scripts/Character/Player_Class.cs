8780a7fc71e72c9e939488c220d088cf45fb6b13
Javsan0424 <A01198909@tec.mx>
2026-09-06 15:41:28 -0600

    First Commit

---
+++ Assets/Scripts/Character/Player_Class.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Player_Class : MonoBehaviour
+{
+
+    public int health;
+    public int MP;
+    public int defense;
+    public int AD;
+    public int AP;
+    public int speed;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Warrior Stats
+        health = 100;
+        MP = 20;
+        defense = 10;
+        AD = 10;
+        AP = 1;
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
